e8a97e75646c8c0d93e0023149114d4ffbda5d27
augusto-dip <augustodip26@gmail.com>
2025-06-10 14:57:14 -0300

    ejercicio calculadora fin

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -1,5 +1,30 @@
-namespace espacioCalculadora; 
-
-class Calculadora {
-    
+namespace espacioCalculadora
+{
+    public class Calculadora {
+        private double dato;
+        public double Resultado
+        {
+            get => dato;
+        }
+        public void Sumar(double termino)
+        {
+            dato = dato + termino;
+        }
+        public void Restar(double termino)
+        {
+            dato = dato - termino;
+        }
+        public void Multiplicar(double termino)
+        {
+            dato = dato * termino;
+        }
+        public void Dividir(double termino)
+        {
+            dato = dato / termino;
+        }
+        public void Limpiar()
+        {
+            dato = 0;
+        }
+    }
 }
