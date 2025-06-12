cabf4a38d2c4f85e5cdb66fe6639b1c792635f1b
augusto-dip <augustodip26@gmail.com>
2025-06-12 00:31:31 -0300

    terminado punto 2

--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -3,5 +3,41 @@
 
 using sisPersonal;
 
+Empleado[] Arreglo = { new Empleado("Roque", "Dip", new DateTime(1963, 12, 21), 'c', new DateTime(2000, 3, 6), 650000, Empleado.Cargos.Ingeniero),
+                       new Empleado("Juan Pablo", "Borges", new DateTime(2003, 12, 4), 's', new DateTime(2019, 5, 30), 650000, Empleado.Cargos.Investigador),
+                       new Empleado("Martina", "Cespedes", new DateTime(1983, 7, 15), 's', new DateTime(2005, 11, 20), 650000, Empleado.Cargos.Administrativo) };
+double totalAPagar = 0;
+int i = 0;
+int proximoAJubilarse = 0;
+foreach (var persona in Arreglo)
+{
+    Console.WriteLine("\n---------------------------------------------------");
+    Console.WriteLine($"Nombre y apellido: {persona.Nombre} {persona.Apellido}");
+    int edadDelEmpleado = persona.Edad();
+    Console.WriteLine($"Tiene {edadDelEmpleado} años.");
+    Console.WriteLine($"Le falta {persona.Jubilarse()} años para jubilarse");
+    Console.WriteLine($"Tiene una antiguedad de {persona.Antiguedad()} años");
+    Console.WriteLine($"Su sueldo final es de $ {persona.salarioFinal()}");
+    Console.WriteLine("---------------------------------------------------");
+    totalAPagar += persona.salarioFinal();
+    if (persona.Jubilarse() < proximoAJubilarse)
+    {
+        proximoAJubilarse = i;
+    }
+    i++;
+}
+
+Console.WriteLine($"Se paga en total a todos los empleados ${totalAPagar}");
+
+Console.WriteLine("\n----------------------------------------------------------------------------------");
+Console.WriteLine($"La persona q más cerca está de jubilarse es la de índice {proximoAJubilarse} en el arreglo");
+Console.WriteLine($"Nombre y apellido: {Arreglo[proximoAJubilarse].Nombre} {Arreglo[proximoAJubilarse].Apellido}");
+int edadDelEmpleadoJub = Arreglo[proximoAJubilarse].Edad();
+Console.WriteLine($"Tiene {edadDelEmpleadoJub} años.");
+Console.WriteLine($"Le falta {Arreglo[proximoAJubilarse].Jubilarse()} años para jubilarse");
+Console.WriteLine($"Tiene una antiguedad de {Arreglo[proximoAJubilarse].Antiguedad()} años");
+Console.WriteLine($"Su sueldo final es de $ {Arreglo[proximoAJubilarse].salarioFinal()}");
+Console.WriteLine("----------------------------------------------------------------------------------");
+
 
 
--- SisPersonal/SisPersonal.cs
+++ SisPersonal/SisPersonal.cs
@@ -1,6 +1,6 @@
 namespace sisPersonal
 {
-    public class Empleado 
+    public class Empleado
     {
         private string nombre;
         private string apellido;
@@ -8,13 +8,13 @@ namespace sisPersonal
         private char estadoCivil;
         private DateTime fechaIngresoLaburo;
         private double sueldoBasico;
-        public enum cargos
+        public enum Cargos
         {
             Administrativo = 0, Ingeniero = 1, Especialista = 2, Investigador = 3
         };
-        private cargos cargo;
+        private Cargos cargo;
 
-        public Empleado (string nombre, string apellido, DateTime fechaNacimiento, char estadoCivil, DateTime fechaIngresoLaburo, double sueldoBasico, cargos cargo)
+        public Empleado(string nombre, string apellido, DateTime fechaNacimiento, char estadoCivil, DateTime fechaIngresoLaburo, double sueldoBasico, Cargos cargo)
         {
             this.nombre = nombre;
             this.apellido = apellido;
@@ -24,10 +24,65 @@ namespace sisPersonal
             this.sueldoBasico = sueldoBasico;
             this.cargo = cargo;
         }
+        public int Edad()
+        {
+            if (System.DateTime.Today.Month > fechaNacimiento.Month)
+            {
+                return System.DateTime.Today.Year - fechaNacimiento.Year;
+            }
+            if (System.DateTime.Today.Month == fechaNacimiento.Month && System.DateTime.Today.Day >= fechaNacimiento.Day)
+            {
+                return System.DateTime.Today.Year - fechaNacimiento.Year;
+            }
+            return System.DateTime.Today.Year - fechaNacimiento.Year - 1;
+        }
 
-        
+        public int Jubilarse()
+        {
+            return 65 - Edad();
+        }
 
-        
+        public int Antiguedad()
+        {
+            if (System.DateTime.Today.Month > fechaIngresoLaburo.Month)
+            {
+                return System.DateTime.Today.Year - fechaIngresoLaburo.Year;
+            }
+            if (System.DateTime.Today.Month == fechaIngresoLaburo.Month && System.DateTime.Today.Day >= fechaIngresoLaburo.Day)
+            {
+                return System.DateTime.Today.Year - fechaIngresoLaburo.Year;
+            }
+            return System.DateTime.Today.Year - fechaIngresoLaburo.Year - 1;
+        }
 
+        public double salarioFinal()
+        {
+            int antiguedad = Antiguedad();
+            double adicional;
+            if (antiguedad < 20)
+            {
+                adicional = 0.01 * antiguedad;
+            }
+            else
+            {
+                adicional = 0.25;
+            }
+
+            if (cargo == Cargos.Ingeniero || cargo == Cargos.Especialista)
+            {
+                adicional *= 1.5;
+            }
+            if (estadoCivil == 'c' || estadoCivil == 'C')
+            {
+                return sueldoBasico * (1 + adicional) + 150000;
+            }
+            else
+            {
+                return sueldoBasico * (1 + adicional);
+            }
+        }
+
+        public string Nombre { get => nombre; set => nombre = value;}
+        public string Apellido { get => apellido; set => apellido = value;}
     }
 }
